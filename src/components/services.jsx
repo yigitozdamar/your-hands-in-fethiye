5687f3de3ab0c4042162e690d50db95ea0011ade
yigitozdamar <yigitozdamar@hotmail.com>
2022-04-12 15:07:17 +0300

    updated for us

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,29 +1,31 @@
 export const Services = (props) => {
   return (
-    <div id='services' className='text-center'>
-      <div className='container'>
-        <div className='section-title'>
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            You can ask us anything about our services. We are here to help you.
+            Local and trustable!
           </p>
         </div>
-        <div className='row'>
+        <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-4'>
-                  {' '}
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
                   <i className={d.icon}></i>
-                  <div className='service-desc'>
-                    <h3>{d.name}</h3>
+                  <div className="service-desc">
+                    <h3>
+                      <strong>{d.name}</strong>
+                    </h3>
                     <p>{d.text}</p>
                   </div>
                 </div>
               ))
-            : 'loading'}
+            : "loading"}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
